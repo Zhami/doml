d65ec352df41b4d9266824b0c887c7861d2c612d
Stuart Malin <stuart@yellowhelium.com>
2011-07-30 15:44:06 -0400

    major refactoring - getting ready to recurse

--- src/doml.js
+++ src/doml.js
@@ -1,16 +1,16 @@
 !function (context) {
 
-	var	contextDoc, createNode, doml, Doml, Domil_orig, env, getGlobal, isArray, isNode, procArgs, procTag;
+	var	Doml, env, isArray, isNode;
 
-//	var sys = require('sys');
-//	var	eyes = require('eyes');
+	var sys = require('sys');
+	var	eyes = require('eyes');
 
 
 	//----------------------------------------
 	// support
 	//----------------------------------------
 	getEnv = function () {
-		var	env, global;
+		var	env;
 		env = {
 			global: undefined,
 			isAMD: false,
@@ -40,97 +40,127 @@
 	};
 
 	//----------------------------------------
-	// ops
+	// Element Constructor & Prototype
 	//----------------------------------------
 
-	createNode = function () {
-		var	element, i, n, s, setAttr;
+	Element = function (doc) {
+		if (!doc) {
+			return null;
+		}
+		this.document = doc;
+		this.element = null;
+		this.tagName = undefined;
+		this.content = '';
+		this.attrs = {};
+		this.elems = [];
+	};
 
-		setAttr = function (elem, name, value) {
+	Element.prototype = (function () {
+		var	createElement, procArg, procArgs;
 
-			switch (name) {
-			case "checked":
-			case "selected":
-				elem[name] = Boolean(value);
-				break;
-			case "className":
-			case "class":
-				elem.className = value;
-				break;
-			default:
-				elem.setAttribute(name, value);
+		//----------------------------------------
+		// private methods
+		//----------------------------------------
+
+		createElement = function () {
+			var	element, i, n, s, setAttr;
+
+			setAttr = function (elem, name, value) {
+
+				switch (name) {
+				case "checked":
+				case "selected":
+					elem[name] = Boolean(value);
+					break;
+				case "className":
+				case "class":
+					elem.className = value;
+					break;
+				default:
+					elem.setAttribute(name, value);
+				}
+			};
+
+			// create the element
+			this.element = element = this.document.createElement(this.tagName);
+
+			// set Attributes
+			s = this.attrs;
+			for (n in s) {
+				s.hasOwnProperty(n) && setAttr(element, n, s[n]);
+			}
+
+			// add text (but not to nodes that don't allow it)
+			s = !/^input$/.test(this.tagName.toLowerCase());
+			if (s && (s =  this.content)) {
+				element.innerHTML = s;
 			}
 		};
 
-		// create the element
-		element = this.document.createElement(this.tagName);
-
-		// set Attributes
-		s = this.attrs;
-		for (n in s) {
-			s.hasOwnProperty(n) && setAttr(element, n, s[n]);
-		}
-
-		// add text (but not to nodes that don't allow it)
-		s = !/^input$/.test(this.tagName.toLowerCase());
-		if (s && (s =  this.content)) {
-			element.innerHTML = s;
-		}
-		return element;
-	};
-
-	getText = function (elem) {
-		var node;
-
-	};
-
-	procArg = function (arg) {
-		var	i, n, ptr, t;
-		t = isArray(arg) ? 'array' : typeof arg;
-		switch (t) {
-		case 'string':
-			this.content += arg;
-			break;
-		case 'array':
-			// elements
-			n = arg.length;
-			ptr = this.elems;
-			for (i = 0; i < n; i += 1) {
-				ptr.push(arg[i]);
-			}
-			break;
-		case 'object':
-			// attributes
-			ptr = this.attrs;
-			for (n in arg) {
-				if (arg.hasOwnProperty(n)) {
-					ptr[n] = arg[n];
+		procArg = function (arg) {
+			var	i, n, ptr, t;
+			t = isArray(arg) ? 'array' : typeof arg;
+			switch (t) {
+			case 'string':
+				this.content += arg;
+				break;
+			case 'array':
+				// elements
+				n = arg.length;
+				ptr = this.elems;
+				for (i = 0; i < n; i += 1) {
+					ptr.push(arg[i]);
 				}
+				break;
+			case 'object':
+				// attributes
+				ptr = this.attrs;
+				for (n in arg) {
+					if (arg.hasOwnProperty(n)) {
+						ptr[n] = arg[n];
+					}
+				}
+				break;
+			case 'function':
+				t = arg.call(this);
+				if (t) {
+					procArg.call(this, t);
+				}
+				break;
 			}
-			break;
-		case 'function':
-			t = arg.call(this);
-			if (t) {
-				procArg.call(this, t);
+		};
+
+		procArgs = function (origArgs) {
+			var	arg, args, i, numArgs;
+
+			args = Array.prototype.slice.call(origArgs, 0);
+			numArgs = args.length;
+			if ((numArgs === 0) || (typeof args[0] !== 'string')) {
+				return;
 			}
-			break;
-		}
-	};
+			this.tagName = args[0];
+			for (i = 1; i < numArgs; i += 1) {
+				arg = args[i];
+				procArg.call(this, arg);
+			}
+		};
 
-	procArgs = function (origArgs) {
-		var	arg, args, i, numArgs;
+		//----------------------------------------
+		// public methods
+		//----------------------------------------
+		return {
+			create: function () {
+				procArgs.call(this, arguments);
+				if (this.tagName) {
+					createElement.apply(this);
+				}
+			},
+			getElement: function () {
+				return this.element;
+			}
+		};
+	})();
 
-		args = Array.prototype.slice.call(origArgs, 0);
-		numArgs = args.length;
-		if ((numArgs === 0) || (typeof args[0] !== 'string')) {
-			return;
-		}
-		this.tagName = args[0];
-		for (i = 1; i < numArgs; i += 1) {
-			arg = args[i];
-			procArg.call(this, arg);
-		}
-	};
 
 	//----------------------------------------
 	// Constructor
@@ -141,36 +171,22 @@
 			doc = env.global.window.document;
 		}
 		this.document = doc;
-		this.clear();
 	};
 
-
 	Doml.prototype = {
-		clear: function () {
-			this.tagName = undefined;
-			this.content = '';
-			this.attrs = {};
-			this.elems = [];
-		},
 
 		create: function () {
-			var	node, rootNode;
-
-			this.clear();
-
+			var	node;
 			if ((arguments.length === 1) && isNode(node = arguments[0])) {
 				return node.cloneNode(true);
 			} else {
-				procArgs.call(this, arguments);
-
-				if (!this.document || !this.tagName) {
-					return null;
-				} else {
-					rootNode = createNode.call(this);
-					return rootNode;
-				}
+				node = new Element(this.document);
+				node.create.apply(node, arguments);
+				return node.getElement();
 			}
 		},
+
+		noConflict: function () {},
 
 		setDocument: function (doc) {
 			this.document = doc;
@@ -183,27 +199,26 @@
 	// init
 	//----------------------------------------
 	env = getEnv();
-	if (context.window) {
-		contextDoc = window.document;
-	}
 
 	//----------------------------------------
 	// setup environment
 	//----------------------------------------
 
-	Domil_orig = context.Doml;
 
 	if (env.isEnder) {
 		module.exports = new Doml();
 	} else if (env.isBrowser) {
-		Doml.noConflict = function () {
-			context.Doml = Domil_orig;
-			return Doml;
-		};
-		context.Doml = Doml;
+		// use immediate annonymous function to create closure
+		!function (context) {
+			var	Domil_orig = context.Doml;
+			context.Doml = Doml;
+			Doml.noConflict = function () {
+				context.Doml = Domil_orig;
+				return Doml;
+			};
+		}(context);
 	} else if (env.isModule) {
 		module.exports = Doml;
-		Doml.noConflict = function () {};
 	} else {
 		throw new Error('Doml: can not determine the environment!');
 	}
